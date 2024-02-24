e3add704c80b9849ca5f6bc7b763bdc5331b796b
Marc Bria <marc.bria@uab.es>
2024-02-24 20:02:36 +0100

    Update

---
+++ call.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget --mirror --convert-links --adjust-extension --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0" https://rehabitemlesruralitats.org
+
+# Not tested
+# wget -m -p -E -k -np https://rehabitemlesruralitats.org
